2a4108c29dda4f2fc334ae041177435215718fa7
unknown <lolipop22234@gmail.com>
2023-09-28 17:16:26 +0300

    Second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
 public class BonusMilesService {
+    public int calculate(int price) {
+        int mile = 20; // Количество рублей для одной бонусной мили
+        int bonus = price / mile; // Рассчёт количества бонусных миль
+     return bonus;
+    }
 }
